943893ed494b5a0f570664018928e34c160600be
1051727403@qq.com <1051727403@qq.com>
2023-07-02 21:51:49 +0800

    2023-7-2 更新

--- src/main/java/com/FDSC/config/Intercptor/JwtInterceptor.java
+++ src/main/java/com/FDSC/config/Intercptor/JwtInterceptor.java
@@ -3,37 +3,31 @@ package com.FDSC.config.Intercptor;
 import cn.hutool.core.util.StrUtil;
 import com.FDSC.common.Constants;
 import com.FDSC.entity.User;
+import com.FDSC.exception.ServiceException;
 import com.FDSC.service.UserService;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.FDSC.exception.ServiceException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
-@Slf4j
 public class JwtInterceptor implements HandlerInterceptor {
     @Autowired
     private UserService userService;
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
         String token = request.getHeader("token");// 从 http 请求头中取出 token
-        System.out.println("---------------------------------------------------------------------"+token);
+        System.out.println("token:"+token);
         if(!(object instanceof HandlerMethod)){
             return true;
         }
         //验证token是否为空
-        if(StrUtil.isBlank(token)){
+        if(StrUtil.isBlank(token)||token==null){
             throw new ServiceException(Constants.CODE_401,"无token，请重新登录！");//权限验证失败
         }
         // 获取 token 中的 user id
