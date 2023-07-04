89425f2fbf518a4103f83ea979682ebd0935e0d0
Ays <2377078993@qq.com>
2023-07-04 14:13:53 +0800

    2023-7-4 首页按钮

--- src/main/java/com/FDSC/controller/AdminController.java
+++ src/main/java/com/FDSC/controller/AdminController.java
@@ -2,13 +2,11 @@ package com.FDSC.controller;
 
 import com.FDSC.common.Constants;
 import com.FDSC.common.Result;
+import com.FDSC.entity.Announcement;
 import com.FDSC.entity.Story;
 import com.FDSC.entity.Tag;
 import com.FDSC.entity.User;
-import com.FDSC.service.AdminService;
-import com.FDSC.service.StoryService;
-import com.FDSC.service.TagService;
-import com.FDSC.service.UserService;
+import com.FDSC.service.*;
 import com.FDSC.utils.TokenUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -29,6 +27,8 @@ public class AdminController {
     private TagService tagService;
     @Autowired
     private StoryService storyService;
+    @Autowired
+    private AnnounceService announceService;
 
     @GetMapping("/userPage")
     public IPage<User> userPage(@RequestParam Integer pageNum,
@@ -102,6 +102,22 @@ public class AdminController {
     @PostMapping("/deleteStory")
     public Result deleteStory(@RequestParam String storyId){
         return adminService.deleteStory(storyId);
+    }
+
+    @GetMapping("/announcePage")
+    public IPage<Announcement> announcePage(@RequestParam Integer pageNum,
+                                            @RequestParam Integer pageSize,
+                                            @RequestParam (defaultValue = "")String search){
+        //若没有传值，则赋默认值，防止将所有数据筛选出来
+        IPage<Story> page=new Page<>(pageNum,pageSize);
+        QueryWrapper queryWrapper=new QueryWrapper<Story>();
+//        queryWrapper.like("username",search);
+//        queryWrapper.or();
+        queryWrapper.like("title",search);
+        queryWrapper.or();
+        queryWrapper.like("content",search);
+        queryWrapper.orderByDesc("id");
+        return storyService.page(page,queryWrapper);
     }
 
 }
