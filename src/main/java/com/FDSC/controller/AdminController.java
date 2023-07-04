8e301ac88f700f0a9e7faea005e60da28ccaf675
1051727403@qq.com <1051727403@qq.com>
2023-07-04 14:08:21 +0800

    2023-7-2 更新

--- src/main/java/com/FDSC/controller/AdminController.java
+++ src/main/java/com/FDSC/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.FDSC.controller;
 
 import com.FDSC.common.Result;
+import com.FDSC.entity.Fragment;
 import com.FDSC.entity.Tag;
+import com.FDSC.entity.User;
 import com.FDSC.service.AdminService;
+import com.FDSC.service.FragmentService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private FragmentService fragmentService;
     @Autowired
     private AdminService adminService;
     @PostMapping("/deleteUser")
@@ -33,5 +41,26 @@ public class AdminController {
 
     @PostMapping("/addTag")
     public Result addOrUpdateTag(@RequestBody Tag tag){return adminService.addOrUpdateTag(tag);}
+
+    @GetMapping("/fragment/page")
+    public IPage<Fragment> findPage(@RequestParam Integer pageNum,
+                                    @RequestParam Integer pageSize,
+                                    @RequestParam (defaultValue = "")String search){
+        //若没有传值，则赋默认值，防止将所有数据筛选出来
+        IPage<Fragment> page=new Page<>(pageNum,pageSize);
+        QueryWrapper queryWrapper=new QueryWrapper<User>();
+        queryWrapper.like("id",search);
+        queryWrapper.or();
+        queryWrapper.like("user_id",search);
+        queryWrapper.or();
+        queryWrapper.like("parent_id",search);
+        queryWrapper.or();
+        queryWrapper.like("fragment_name",search);
+        queryWrapper.or();
+        queryWrapper.like("content",search);
+        queryWrapper.orderByDesc("id");
+        //后台获取用户登录信息
+        return fragmentService.page(page,queryWrapper);
+    }
 
 }
